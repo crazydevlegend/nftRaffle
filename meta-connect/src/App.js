93d6c8b7423a38182f7214e87fbe6fbb7195cb97
kkirico <yasingco@naver.com>
2021-12-17 07:56:43 +0900

    Add files via upload

---
+++ meta-connect/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import WalletCard from './WalletCard'; 
+function App() {
+  return (
+    <div className="App">
+      <WalletCard></WalletCard>
+    </div>
+  );
+}
+
+export default App;
